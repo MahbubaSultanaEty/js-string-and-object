a6ea5791bed39288d1a8b6d9fd4fe80637313780
Mahbuba-S-Ety <sultanamahbuba09@gmail.com>
2026-01-30 20:15:36 +0600

    object

---
+++ object.js
@@ -0,0 +1,24 @@
+const age = 21;
+const school = "girls school";
+const isPassed = true;
+const subject = ['bangla', 'english', 'math'];
+
+// An object groups properties (data) and methods (functions) into one thing.
+
+const mobile = {
+    brand: "moto",
+    price: 35000,
+    color: "seaGreen",
+    isFast: true
+}
+
+const dress= {
+    color: "maroon",
+    price: 2300,
+    name: 'anarkali',
+    size: [44, 56, 87],
+    quality: {
+        fabrics: 'cotton',
+        isSustainable: true,
+    }
+}
